3c3a71d099ec83d0a6c9633222c63d7dce1c6d1f
Russ Christensen <russ-christensen@live.com>
2024-10-01 20:26:56 -0700

    Add examples that cause problems and show bugs

--- examples/select.rs
+++ examples/select.rs
@@ -2,6 +2,7 @@ use inquire::Select;
 
 fn main() {
     let options = vec![
+        " [🏃‍♂️ Needs a reason] |  Some text to use up space  ⬅  More text to use up space ⬅  More and more and more text to use up space ⬅  More and more text to use up space ⬅  More and more text to use up space ⬅  More text to use up space ⬅  More Text to use up space ⬅  More text to use up space ⬅  More text to use up space ⬅  Final text section",
         "Banana",
         "Apple",
         "Strawberry",
@@ -13,7 +14,14 @@ fn main() {
         "Pear",
         "Avocado",
         "Pineapple",
-    ];
+        //"Download - school webpage - https://web.archive.org/web/20150910082528/http://www.cs.utah.edu/~rchriste/  ⬅ 🎯🏞 Save a history of my life / Journaling / Family History ⬅ 🎯 Save a history of my life / Journaling",
+//        "🔥 [👨‍👦 Needs a reason] |🔴 🪜 Some text to use up space  ⬅ 🪜 More text to use up space ⬅ 🪜 More and more and more text to use up space ⬅ 🪜 More and more text to use up space ⬅ 🪜 More and more text to use up space ⬅ 🪜 More text to use up space ⬅ 🪜 More Text to use up space ⬅ 🪜 More text to use up space ⬅ 🪜 More text to use up space ⬅ 🎯 Final text section",
+// "🔥 [🏗️ Needs a reason] |🔴 🪜 Some text to use up space  ⬅ 🪜 More text to use up space ⬅ 🪜 More and more and more text to use up space ⬅ 🪜 More and more text to use up space ⬅ 🪜 More and more text to use up space ⬅ 🪜 More text to use up space ⬅ 🪜 More Text to use up space ⬅ 🪜 More text to use up space ⬅ 🪜 More text to use up space ⬅ 🎯 Final text section",
+//This next one works fine
+//"🔥 [🏃 Needs a reason] |🔴 🪜 Some text to use up space  ⬅ 🪜 More text to use up space ⬅ 🪜 More and more and more text to use up space ⬅ 🪜 More and more text to use up space ⬅ 🪜 More and more text to use up space ⬅ 🪜 More text to use up space ⬅ 🪜 More Text to use up space ⬅ 🪜 More text to use up space ⬅ 🪜 More text to use up space ⬅ 🎯 Final text section",
+//         "🔥 [🏃‍♂️ Needs a reason] |🔴 🪜 Some text to use up space  ⬅ 🪜 More text to use up space ⬅ 🪜 More and more and more text to use up space ⬅ 🪜 More and more text to use up space ⬅ 🪜 More and more text to use up space ⬅ 🪜 More text to use up space ⬅ 🪜 More Text to use up space ⬅ 🪜 More text to use up space ⬅ 🪜 More text to use up space ⬅ 🎯 Final text section",
+
+     ];
 
     let ans = Select::new("What's your favorite fruit?", options.clone()).prompt();
 
